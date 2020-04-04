4ff287deeab492eb961174a5d52e24068fc706d9
iam4x <iam4x@me.com>
2020-04-04 15:08:15 +0200

    fix(web/index): wrap into `withApollo`

--- packages/web/pages/index.tsx
+++ packages/web/pages/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
+import { withApollo } from '../components/with-apollo';
 import { LayoutComponent } from '../components/layout/layout.component';
 import { HomeComponent } from '../components/home/home.component';
 
-export default function IndexPage() {
+function IndexPage() {
   return (
     <LayoutComponent>
       <HomeComponent />
     </LayoutComponent>
   );
 }
+
+export default withApollo({ ssr: false })(IndexPage);
