9ef25f41d75aa5d6db41b767bd90df38be203146
iam4x <iam4x@me.com>
2020-04-06 00:46:36 +0200

    chore(api): mount jobs ui to /jobs

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use('/admin/queues', UI);
+  app.use('/jobs', UI);
   await app.listen(4000);
 }
 bootstrap();
