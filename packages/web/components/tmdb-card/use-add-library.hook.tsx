b7d7bf59147f8b0af74e786e1bee72edd24e6133
iam4x <iam4x@me.com>
2020-04-10 19:39:42 +0200

    fix(missing): reload when add or remove

--- packages/web/components/tmdb-card/use-add-library.hook.tsx
+++ packages/web/components/tmdb-card/use-add-library.hook.tsx
@@ -7,6 +7,7 @@ import {
   EnrichedMovie,
   GetLibraryMoviesDocument,
   GetDownloadingDocument,
+  GetMissingDocument,
 } from '../../utils/graphql';
 
 export function useAddLibrary({
@@ -21,6 +22,7 @@ export function useAddLibrary({
     refetchQueries: [
       { query: GetLibraryMoviesDocument },
       { query: GetDownloadingDocument },
+      { query: GetMissingDocument },
     ],
     onError: ({ message }) =>
       notification.error({
--- packages/web/components/tmdb-card/use-remove-library.hook.tsx
+++ packages/web/components/tmdb-card/use-remove-library.hook.tsx
@@ -7,6 +7,7 @@ import {
   GetLibraryMoviesDocument,
   useRemoveMovieMutation,
   GetDownloadingDocument,
+  GetMissingDocument,
 } from '../../utils/graphql';
 
 export function useRemoveLibrary({
@@ -21,6 +22,7 @@ export function useRemoveLibrary({
     refetchQueries: [
       { query: GetLibraryMoviesDocument },
       { query: GetDownloadingDocument },
+      { query: GetMissingDocument },
     ],
     onError: ({ message }) =>
       notification.error({
