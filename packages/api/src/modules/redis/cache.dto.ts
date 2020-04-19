af0c457af3adabc4bd9dd619dd12433d7789839c
z7shahriar <z7.shahriary@gmail.com>
2020-04-19 22:48:35 +0200

    chore: typo in cache dto

--- packages/api/src/modules/redis/cache.dto.ts
+++ packages/api/src/modules/redis/cache.dto.ts
@@ -2,5 +2,5 @@ export enum CacheKeys {
   RECOMMENDED_TV_SHOWS = 'recommended_tv_shows',
   RECOMMENDED_MOVIES = 'recommended_movies',
   TMDB_LANGUAGES = 'tmdb_languages',
-  TMDB_GENRES = 'tmdb_geres',
+  TMDB_GENRES = 'tmdb_genres',
 }
