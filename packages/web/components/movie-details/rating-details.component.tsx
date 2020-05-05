4a5213a0bab5e5b75b4a3af501143e22cd071838
z7shahriar <z7.shahriar@gmail.com>
2020-05-05 19:00:57 +0200

    feat: Added OMDB for tvshows

--- packages/web/components/movie-details/rating-details.component.tsx
+++ packages/web/components/movie-details/rating-details.component.tsx
@@ -6,18 +6,18 @@ import {
 } from '../../utils/graphql';
 import { RatingDetailsStyles } from './rating-details.styles';
 export const RatingDetailComponent = ({
-  movie,
+  entertainment,
 }: {
-  movie: TmdbSearchResult | EnrichedMovie;
+  entertainment: TmdbSearchResult | EnrichedMovie;
 }) => {
   const { data } = useOmdbSearchQuery({
-    variables: { title: movie.title },
+    variables: { title: entertainment.title },
   });
 
   const ratings = data?.result.ratings;
 
   const allRatings = {
-    TMDB: `${movie.voteAverage * 10}%`,
+    TMDB: `${entertainment.voteAverage * 10}%`,
     IMDB: ratings?.IMDB,
     rottenTomatoes: ratings?.rottenTomatoes,
     metaCritic: ratings?.metaCritic,
@@ -31,7 +31,7 @@ export const RatingDetailComponent = ({
         if (!rate) return null;
 
         return (
-          <li>
+          <li key={entertainment.tmdbId}>
             <img src={`/assets/rating/${key}.png`} />
             <span className="rating-details--rate">{rate?.[0]}</span>
             <span className="rating-details--rate-suffix">{rate?.[1]}</span>
--- packages/web/components/tvshow-details/tvshow-details.component.tsx
+++ packages/web/components/tvshow-details/tvshow-details.component.tsx
@@ -17,6 +17,7 @@ import { getImageURL } from '../../utils/get-cached-image-url';
 import { RatingComponent } from '../rating/rating.component';
 import { useGetSeasons } from './use-get-seasons.hook';
 import { TVShowSeasonsModalComponentStyles } from './tvshow-details.styles';
+import { RatingDetailComponent } from '../movie-details/rating-details.component';
 
 interface TVShowSeasonsModalComponentProps {
   visible: boolean;
@@ -115,7 +116,7 @@ export function TVShowSeasonsModalComponent(
                 )}
               </div>
               <div className="information-row">
-                <RatingComponent rating={tvShow.voteAverage * 10} />
+                <RatingDetailComponent entertainment={tvShow} />
                 <a
                   className="play-trailer btn"
                   href={youtubeSearchURL}
