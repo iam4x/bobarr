f4656d35414651eab3aad3975511c26b6b35f80d
iam4x <iam4x@me.com>
2020-08-27 10:46:22 +0200

    chore(api): upgrade deps

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { WinstonModule } from 'nest-winston';
-import { UI } from 'bull-board';
+import { router as bullBoardMiddleware } from 'bull-board';
 
 import { AppModule } from './app.module';
 import { winstonOptions } from './utils/winston-options';
@@ -8,7 +8,7 @@ import { winstonOptions } from './utils/winston-options';
 async function bootstrap() {
   const logger = WinstonModule.createLogger(winstonOptions);
   const app = await NestFactory.create(AppModule, { logger });
-  app.use('/jobs', UI);
+  app.use('/jobs', bullBoardMiddleware);
   await app.listen(4000);
 }
 bootstrap();
